cecb122a2b33f8af3435a080f077eb46abb91fc3
Lokuyow <94349922+Lokuyow@users.noreply.github.com>
2025-07-26 13:06:38 +0900

    Viteの設定ファイルを削除し、PWAプラグインの設定を整理しました。

--- my-svelte-app/vite.config.ts
+++ my-svelte-app/vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  base: '/ehagaki/',
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'eHagaki',
+        short_name: 'eHagaki',
+        description: '軽量なポストオンリー型Nostrクライアントで、デバイス上で画像を圧縮することにより高速かつデータ効率に優れたアップロードを実現します。',
+        theme_color: '#699f43ff',
+        icons: [
+          {
+            src: '/src/assets/hagaki_2mai.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/src/assets/hagaki_2mai.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
