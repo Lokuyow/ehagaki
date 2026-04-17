26119cfa21ce415b35772f4845d41cf825290bc4
Lokuyow <94349922+Lokuyow@users.noreply.github.com>
2026-04-17 20:35:11 +0900

    PRECACHE_VERSIONを'1.15.0'から'1.15.1'に更新しました

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@ import {
 } from "../src/lib/profilePictureUrlUtils";
 
 // 定数定義
-const PRECACHE_VERSION = '1.15.0';
+const PRECACHE_VERSION = '1.15.1';
 const PRECACHE_NAME = `ehagaki-cache-${PRECACHE_VERSION}`;
 const PROFILE_CACHE_NAME = 'ehagaki-profile-images';
 const INDEXEDDB_NAME = 'eHagakiSharedData';
