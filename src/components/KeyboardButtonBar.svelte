e2526223ff11efa4982dc22661d21a75efbcf7e7
Lokuyow <94349922+Lokuyow@users.noreply.github.com>
2026-04-30 00:23:27 +0900

    ボタンのスタイルからボーダー半径の設定を削除しました。

--- src/components/KeyboardButtonBar.svelte
+++ src/components/KeyboardButtonBar.svelte
@@ -472,7 +472,7 @@
             width: clamp(40px, 22vw, 100px);
             height: var(--keyboard-button-bar-height);
             padding: 0 6px;
-            border-radius: 4px;
+            /* border-radius: 4px; */
 
             &:active {
                 transform: scale(0.9);
