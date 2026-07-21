532fe0b34a0beb3d7d27a2864559889da6284064
Lokuyow <cg.magnet@gmail.com>
2026-07-22 02:24:22 +0900

    リファクタリング: sw-update-btn のパディングを調整

--- src/components/SettingsDialog.svelte
+++ src/components/SettingsDialog.svelte
@@ -824,7 +824,7 @@
     :global(.sw-update-btn.primary) {
         height: 54px;
         width: auto;
-        padding: 12px 10px 12px;
+        padding: 12px 10px 12px 8px;
         flex-shrink: 0;
     }
 
