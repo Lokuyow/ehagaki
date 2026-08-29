4e770698c980cf45a2ccdb4238d5ef8afbcc71e4
Lokuyow <cg.magnet@gmail.com>
2026-08-29 16:33:44 +0900

    Updates

---
+++ web-component/assets/hidden-input-B585v2I-.js
@@ -0,0 +1,38 @@
+import { I as _, K as c, aT as h, dC as g, aS as u, aU as y } from "./App-BXRUDZkJ.js";
+import { b0 as x, b1 as $, b2 as P, a as s, b3 as i, b4 as S, aR as k, b5 as H, ba as n, b6 as C } from "./entry-P10b_OwJ.js";
+import { b as I } from "./input-DAT85sFW.js";
+var K = n("<input/>"), O = n("<input/>");
+function R(l, r) {
+  x(r, !0);
+  let t = _(r, "value", 15), p = y(r, ["$$slots", "$$events", "$$legacy", "$$host", "value"]);
+  const o = k(() => h(p, {
+    "aria-hidden": "true",
+    tabindex: -1,
+    style: { ...g, position: "absolute", top: "0", left: "0" }
+  }));
+  var d = {
+    get value() {
+      return t();
+    },
+    set value(e) {
+      t(e), H();
+    }
+  }, v = $(), m = P(v);
+  {
+    var b = (e) => {
+      var a = K();
+      u(a, () => ({ ...s(o), value: t() }), void 0, void 0, void 0, void 0, !0), i(e, a);
+    }, f = (e) => {
+      var a = O();
+      u(a, () => ({ ...s(o) }), void 0, void 0, void 0, void 0, !0), I(a, t), i(e, a);
+    };
+    c(m, (e) => {
+      s(o).type === "checkbox" ? e(b) : e(f, -1);
+    });
+  }
+  return i(l, v), S(d);
+}
+C(R, { value: {} }, [], [], { mode: "open" });
+export {
+  R as H
+};
--- web-component/ehagaki-composer.js
+++ web-component/ehagaki-composer.js
@@ -1,4 +1,4 @@
-import { bI as _, bJ as a, bK as I } from "./assets/entry-BQ9RlsLv.js";
+import { bI as _, bJ as a, bK as I } from "./assets/entry-P10b_OwJ.js";
 export {
   _ as EHAGAKI_COMPOSER_API_VERSION,
   a as EHAGAKI_COMPOSER_TAG_NAME,
