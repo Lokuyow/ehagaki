e71d0a175e869ebb1c518becc293ae913a390825
Lokuyow <cg.magnet@gmail.com>
2026-09-09 17:16:12 +0900

    Updates

--- web-component/ehagaki-composer.js
+++ web-component/ehagaki-composer.js
@@ -1,4 +1,4 @@
-import { c1 as _, c2 as a, c3 as O } from "./assets/entry-BmNX31GD.js";
+import { c1 as _, c2 as a, c3 as O } from "./assets/entry-rRm6YwMC.js";
 export {
   _ as EHAGAKI_COMPOSER_API_VERSION,
   a as EHAGAKI_COMPOSER_TAG_NAME,
--- web-component/host-owned/ehagaki-composer.js
+++ web-component/host-owned/ehagaki-composer.js
@@ -1,4 +1,4 @@
-import { bq as _, br as a, bs as s } from "./assets/host-owned-entry-B6Y4DLVu.js";
+import { bq as _, br as a, bs as s } from "./assets/host-owned-entry-BElPnyYu.js";
 export {
   _ as EHAGAKI_COMPOSER_API_VERSION,
   a as EHAGAKI_COMPOSER_TAG_NAME,
